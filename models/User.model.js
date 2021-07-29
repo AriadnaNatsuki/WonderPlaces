f95c150bc0a6b991daf4ec2376038b107359799a
Nicolas del Brio Arias <nicodelbrio@outlook.es>
2021-07-29 22:22:55 +0200

    Arreglando errores perfil

--- models/User.model.js
+++ models/User.model.js
@@ -19,18 +19,10 @@ const userSchema = new mongoose.Schema(
             required: [true, "Password is required"],
             minlength: [8, "Password is too short"]
         },
-        imgPath: {
-            type: String,
-            default: 'https://res.cloudinary.com/di76jljny/image/upload/v1625748116/360_F_410437733_hdq4Q3QOH9uwh0mcqAhRFzOKfrCR24Ta_ptk3pp.jpg'
-          },
-          imgName: {
-            type: String,
-            default: 'default-user-pic'
-          });
-    }
     {
         timestamps: true
     }
+});
     
 
 userSchema.pre("save", function (next) {
